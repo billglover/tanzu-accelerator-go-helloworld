5f156449df4c7faa0e5a839620e36ea9682fec60
Bill Glover <bglover@vmware.com>
2023-05-31 11:37:21 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module tanzu-go-helloworld
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	err := run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(res, "Tanzu does Go too!")
+	})
+
+	err := http.ListenAndServe(":8080", mux)
+	return err
+}
